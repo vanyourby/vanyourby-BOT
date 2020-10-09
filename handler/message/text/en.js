26af1bdd98f53214529c61f7bdd867d6b7c94096
vanyourby <72586909+vanyourby@users.noreply.github.com>
2020-10-09 09:24:01 +0700

    Update en.js

--- handler/message/text/en.js
+++ handler/message/text/en.js
@@ -13,10 +13,10 @@ By using this source code / bot, you agree to the following Terms and Conditions
     • burning forest/ deforestation
     • hate speech or discrimination based on age, sex, gender identity, race, sexuality, religion, nationality
 
-Source Code BOT: https://github.com/YogaSakti/imageToSticker
+Source Code BOT: https://github.com/vanyourby/vanyourby-BOT
 NodeJS WhatsApp library: https://github.com/open-wa/wa-automate-nodejs
 
-Best regards, Yoga Sakti.`
+Best regards, @vanyourby.`
 }
 
 exports.textMenu = (pushname) => {
